cfe6d181b2dc7f5b16884d3f34cd7f0f73830918
mahmud aremu <aremumahmud2003@gmail.com>
2024-07-05 10:57:03 +0100

    dd

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 const jwt = require('jsonwebtoken');
 
-// Connect to Redis
-const redisClient = require('./clients/redis.client');
-const socketIo = require('socket.io');
+
 const app = require('./app');
 const http = require('http');
-const sendMessageController = require('./controller/sendMessage.controller');
-const authMiddleware = require('./middleware/authMiddleware');
-const deleteMessageController = require('./controller/deleteMessage.controller');
+
 
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -33,46 +29,8 @@ mongoose.connect(process.env.mongoURI || 'mongodb://localhost:27017/invest', {
 
 
 
-// Socket.IO connection handling
-io.on('connection', (socket) => {
-    // Register user with their socket ID in Redis
-
-    socket.on('register', (token) => {
-
-        jwt.verify(token, process.env.JWT_SECRET || 'your_secret_key', async(err, user) => {
-            if (err) {
-                return socket.emit('registrationError', true)
-            }
-
-            let { userId } = user
-            let f = await redisClient.sadd(userId, socket.id);
-
-            // console.log(f, await redisClient.smembers(userId))
-            // Remove user from Redis when socket disconnects
-            socket.on('disconnect', () => {
-                redisClient.smembers(userId, async(err, sockets) => {
-                    if (sockets.length === 1 && sockets[0] === socket.id) {
-                        await redisClient.del(userId);
-                    } else {
-                        // console.log(socket.id)
-                        await redisClient.srem(userId, socket.id);
-                    }
-                });
-            });
-        });
-
-    });
 
 
-});
-
-
-
-
-
-app.post('/message', authMiddleware, sendMessageController(io))
-
-app.post('/delete/:id', authMiddleware, deleteMessageController(io))
 
 
 server.listen(3000, () => {
